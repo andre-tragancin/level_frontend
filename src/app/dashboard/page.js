35af6193f125809b834753ef5e1e331a03f97ecc
André Tragancin <altfxd@gmail.com>
2024-09-17 22:28:04 -0300

    change dashboard page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,3 @@
-// import { SideBar } from "../components/Sidebar/Sidebar"
-import { Main } from "../components/Main"
-import { LayoutDashboard, Home, ArrowDownUp, BarChartBig, BarChart4, ClipboardCheck, CalendarDays, Settings, NotebookPen, Menu, MenuIcon } from 'lucide-react';
-import { Sidebar, SubMenu } from "react-pro-sidebar";
-import { SideBar } from "../components/Sidebar/Sidebar";
-
 export default function Dashboard(){
 
     return(
