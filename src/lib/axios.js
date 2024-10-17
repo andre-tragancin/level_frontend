0dfc39892155e06c2bb87ad6b55b694e8e11875f
André Tragancin <altfxd@gmail.com>
2024-10-16 23:10:51 -0300

    changing baseurl

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://216.238.100.28:8004/api/',  // URL base da API
+  // baseURL: 'http://216.238.100.28:8004/api/',  // URL base da API
+  baseURL: 'https://cors-anywhere.herokuapp.com/http://216.238.100.28:8004/api/'
 
 });
 
