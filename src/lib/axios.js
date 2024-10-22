51a0c10177b6d47218e0ce7377c754fd561364f4
André Tragancin <altfxd@gmail.com>
2024-10-21 23:16:00 -0300

    update server url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://216.238.100.28:8004/api/',  // URL base da API
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://216.238.100.28:8004/api/'
+  // baseURL: 'http://localhost:8000/api/',  // URL base da API
+  baseURL: 'https://www.level-backend.me/api/',  // URL base da API
+  // baseURL: 'https://cors-anywhere.herokuapp.com/http://216.238.100.28:8004/api/'
 
 });
 
